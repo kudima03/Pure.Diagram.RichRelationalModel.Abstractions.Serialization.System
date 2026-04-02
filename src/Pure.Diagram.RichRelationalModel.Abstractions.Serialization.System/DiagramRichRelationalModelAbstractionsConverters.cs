7a184d5aa02615e97bc2759e102200fb1e789bca
kudima03 <kudima03@gmail.com>
2026-04-02 22:50:45 +0300

    Add DiagramRichRelationalModelAbstractionsConverters

---
+++ src/Pure.Diagram.RichRelationalModel.Abstractions.Serialization.System/DiagramRichRelationalModelAbstractionsConverters.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Text.Json.Serialization;
+
+namespace Pure.Diagram.RichRelationalModel.Abstractions.Serialization.System;
+
+public sealed class DiagramRichRelationalModelAbstractionsConverters
+    : IEnumerable<JsonConverter>
+{
+    public IEnumerator<JsonConverter> GetEnumerator()
+    {
+        yield return new DiagramTypeRichRelationalModelConverter();
+        yield return new SeriesRichRelationalModelConverter();
+        yield return new DiagramRichRelationalModelConverter();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
